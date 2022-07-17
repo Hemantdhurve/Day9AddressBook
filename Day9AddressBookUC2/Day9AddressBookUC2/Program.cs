77e6b5b01314413ed070e0a73e622415d3e965cc
Hemantdhurve <hemantdhurve007@gmail.com>
2022-07-18 01:31:33 +0530

    Added Day9 AddressBook file

---
+++ Day9AddressBookUC2/Day9AddressBookUC2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Day9AddressBook;
+
+namespace Day9AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            ///Creates a reference of AddressBook
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact();
+        }
+    }
+}
